e59309e37d4d921e18b2068f6668104e5e81e1a9
Leigh Johnson <hi@leighjohnson.me>
2019-11-28 19:48:31 +0000

    lint pid.py

--- rpi_deep_pantilt/control/pid.py
+++ rpi_deep_pantilt/control/pid.py
@@ -1,49 +1,52 @@
 # import necessary packages
 import time
 
+
 class PIDController:
-	def __init__(self, kP=1, kI=0, kD=0):
+    def __init__(self, kP=1, kI=0, kD=0):
 
-		# initialize gains
-		self.kP = kP
-		self.kI = kI
-		self.kD = kD
+        # initialize gains
+        self.kP = kP
+        self.kI = kI
+        self.kD = kD
 
-	def reset(self):
-		# intialize the current and previous time
-		self.time_curr = time.time()
-		self.time_prev = self.time_curr
+    def reset(self):
+        # intialize the current and previous time
+        self.time_curr = time.time()
+        self.time_prev = self.time_curr
 
-		# initialize the previous error
-		self.error_prev = 0
+        # initialize the previous error
+        self.error_prev = 0
 
-		# initialize the term result variables
-		self.cP = 0
-		self.cI = 0
-		self.cD = 0
+        # initialize the term result variables
+        self.cP = 0
+        self.cI = 0
+        self.cD = 0
 
-	def update(self, error):
-		# grab the current time and calculate delta time / error
-		self.time_curr = time.time()
-		time_delta = self.time_curr - self.time_prev
-		error_deltar = error - self.error_prev
+    def update(self, error, sleep=0.01):
 
-		# proportional term
-		self.cP = error
+        time.sleep(sleep)
+        # grab the current time and calculate delta time / error
+        self.time_curr = time.time()
+        time_delta = self.time_curr - self.time_prev
+        error_delta = error - self.error_prev
 
-		# integral term
-		self.cI += error * time_delta
+        # proportional term
+        self.cP = error
 
-		# derivative term and prevent divide by zero
-		self.cD = (error_deltar / time_delta) if time_delta > 0 else 0
+        # integral term
+        self.cI += error * time_delta
 
-		# save previous time and error for the next update
-		self.time_prev = self.time_curr
-		self.error_prev = error
+        # derivative term and prevent divide by zero
+        self.cD = (error_delta / time_delta) if time_delta > 0 else 0
 
-		# sum the terms and return
-		return sum([
-			self.kP * self.cP,
-			self.kI * self.cI,
-			self.kD * self.cD]
+        # save previous time and error for the next update
+        self.time_prev = self.time_curr
+        self.error_prev = error
+
+        # sum the terms and return
+        return sum([
+            self.kP * self.cP,
+            self.kI * self.cI,
+            self.kD * self.cD]
         )
