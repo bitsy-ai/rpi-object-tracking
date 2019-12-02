d2f15181c21eb70f9bea4df36b249272d28da461
Leigh Johnson <hi@leighjohnson.me>
2019-12-02 02:03:07 +0000

    fix long description format

--- setup.py
+++ setup.py
@@ -107,6 +107,7 @@ setup(
     install_requires=requirements,
     license="MIT license",
     long_description=readme + '\n\n' + history,
+    long_description_content_type='text/markdown',
     include_package_data=True,
     keywords='computer vision cv tensorflow raspberrypi detection tracking ',
     name='rpi_deep_pantilt',
@@ -118,4 +119,5 @@ setup(
     url='https://github.com/leigh-johnson/rpi_deep_pantilt',
     version='1.0.0rc',
     zip_safe=False,
+
 )
