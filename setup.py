3674228b6d8a8eafa2af39bd917d1711b3fe5e30
Leigh Johnson <hi@leighjohnson.me>
2019-12-04 06:03:22 +0000

    Bump version: 1.0.0rc5 → 1.0.0

--- setup.py
+++ setup.py
@@ -111,7 +111,7 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/leigh-johnson/rpi_deep_pantilt',
-    version='1.0.0rc5',
+    version='1.0.0',
     zip_safe=False,
 
 )
