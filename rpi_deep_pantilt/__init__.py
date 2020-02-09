bda75131eab8b94ae15b5cb1406b3b9f4aaa8102
Leigh Johnson <hi@leighjohnson.me>
2020-02-09 20:16:29 +0000

    Bump version: 1.0.1 → 1.1.0

--- rpi_deep_pantilt/__init__.py
+++ rpi_deep_pantilt/__init__.py
@@ -4,4 +4,4 @@
 
 __author__ = """Leigh Johnson"""
 __email__ = 'hi@leighjohnson.me'
-__version__ = '1.0.1'
+__version__ = '1.1.0'
--- setup.py
+++ setup.py
@@ -111,7 +111,7 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/leigh-johnson/rpi-deep-pantilt',
-    version='1.0.1',
+    version='1.1.0',
     zip_safe=False,
 
 )
