c04f4ab2045516948a5c09f580f85b716f4066df
Leigh Johnson <hi@leighjohnson.me>
2020-07-25 20:26:08 +0100

    Bump version: 1.2.0 → 1.2.1

--- setup.py
+++ setup.py
@@ -70,7 +70,7 @@ TRAINER_DARWIN_CUSTOM_COMMANDS = [['brew', 'update'],
 class PostInstall(install):
 
     def run(self):
-        deps = 'https://github.com/leigh-johnson/Tensorflow-bin/blob/master/tensorflow-2.0.0-cp37-cp37m-linux_armv7l.whl?raw=true'
+        deps = 'https://github.com/leigh-johnson/Tensorflow-bin/releases/download/v2.2.0/tensorflow-2.2.0-cp37-cp37m-linux_armv7l.whl'
 
         install.run(self)
         # https://pip.pypa.io/en/stable/user_guide/#using-pip-from-your-program
@@ -111,7 +111,7 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/leigh-johnson/rpi-deep-pantilt',
-    version='1.2.0',
+    version='1.2.1',
     zip_safe=False,
 
 )
